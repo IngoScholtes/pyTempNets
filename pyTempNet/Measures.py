bd40a1711a904b258f1cbd3539ef20c17f60f972
Roman Cattaneo <romanc@ethz.ch>
2015-07-07 11:12:46 +0200

    nice docstrings, raising ValueErrors instead of assertions, cleaning intendation in Measures.py

--- pyTempNet/Measures.py
+++ pyTempNet/Measures.py
@@ -17,8 +17,16 @@ from pyTempNet.Processes import *
 def Laplacian(temporalnet, model="SECOND", sparseLA=False, transposed=False):
     """Returns the Laplacian matrix corresponding to the the second-order (model=SECOND) or 
     the second-order null (model=NULL) model for a temporal network.
+    
+    @param temporalnet: The temporalnetwork instance to work on
+    @param model: either C{"SECOND"} or C{"NULL"}, where C{"SECOND"} is the 
+      the default value.
+    @param sparseLA: whether or not (default) to use sparse linear algebra for 
+      calculations.
+    @param transposed: wheter or not (default) to transpose the matrix
     """
-    assert model is "SECOND" or "NULL"
+    if (model is "SECOND" or "NULL") == False:
+        raise ValueError("model must be one of \"SECOND\" or \"NULL\"")
     start = tm.clock()
     
     if model == "SECOND":
@@ -42,8 +50,13 @@ def FiedlerVector(temporalnet, model="SECOND"):
     """Returns the Fiedler vector of the second-order (model=SECOND) or the
     second-order null (model=NULL) model for a temporal network. The Fiedler 
      vector can be used for a spectral bisectioning of the network.
+     
+    @param temporalnet: The temporalnetwork instance to work on
+    @param model: either C{"SECOND"} or C{"NULL"}, where C{"SECOND"} is the 
+      the default value.
     """
-    assert model is "SECOND" or "NULL"
+    if (model is "SECOND" or "NULL") == False:
+        raise ValueError("model must be one of \"SECOND\" or \"NULL\"")
     
     # NOTE: The transposed matrix is needed to get the "left" eigen vectors
     L = Laplacian(temporalnet, model, sparseLA=True, transposed=True)
@@ -62,7 +75,15 @@ def AlgebraicConn(temporalnet, model="SECOND"):
     """Returns the Fiedler vector of the second-order (model=SECOND) or the
     second-order null (model=NULL) model for a temporal network. The Fiedler 
      vector can be used for a spectral bisectioning of the network.
+     
+     @param temporalnet: The temporalnetwork to work on
+     @param model: either C{"SECOND"} or C{"NULL"}, where C{"SECOND"} is the 
+      the default value.
     """
+    
+    if (model is "SECOND" or "NULL") == False:
+        raise ValueError("model must be one of \"SECOND\" or \"NULL\"")
+    
     L = Laplacian(temporalnet, model, sparseLA=True, transposed=True)
     # NOTE: ncv=13 sets additional auxiliary eigenvectors that are computed
     # NOTE: in order to be more confident to find the one with the largest
@@ -74,8 +95,12 @@ def AlgebraicConn(temporalnet, model="SECOND"):
 
 
 def EntropyGrowthRate(T):
-    """Computes the entropy growth rate of a transition matrix"""
+    """Computes the entropy growth rate of a transition matrix
+    
+    @param T: Transition matrix in sparse format."""
 
+    if sparse.issparse(T) == False:
+        raise TypeError("T must be a sparse matrix")
     # Compute normalized leading eigenvector of T (stationary distribution)
     # NOTE: ncv=13 sets additional auxiliary eigenvectors that are computed
     # NOTE: in order to be more confident to find the one with the largest
@@ -90,45 +115,54 @@ def EntropyGrowthRate(T):
     # NOTE: as we work with a sparse matrix here, where the zero elements
     # NOTE: are not stored
     T.data *=  np.log2(T.data)
+    
     return -np.sum( T * pi )
     
     
 def EntropyGrowthRateRatio(t, mode='FIRSTORDER'):
-        """Computes the ratio between the entropy growth rate ratio between
-        the second-order and first-order model of a temporal network t. Ratios smaller
-        than one indicate that the temporal network exhibits non-Markovian characteristics"""
-        
-        # Generate strongly connected component of second-order networks
-        start = tm.clock()
-        g2 = t.igraphSecondOrder().components(mode="STRONG").giant()
-        
-        if mode == 'FIRSTORDER':
-            g2n = t.igraphFirstOrder().components(mode="STRONG").giant()
-        else:
-            g2n = t.igraphSecondOrderNull().components(mode="STRONG").giant()
-        
-        # Calculate transition matrices
-        T2 = Processes.RWTransitionMatrix(g2, sparseLA=True, transposed=True)
-        T2n = Processes.RWTransitionMatrix(g2n, sparseLA=True, transposed=True)
+    """Computes the ratio between the entropy growth rate ratio between
+    the second-order and first-order model of a temporal network t. Ratios smaller
+    than one indicate that the temporal network exhibits non-Markovian characteristics"""
+    
+    # NOTE to myself: most of the time here goes into computation of the
+    # NOTE            EV of the transition matrix for the bigger of the
+    # NOTE            two graphs below (either 2nd-order or 2nd-order null)
+    
+    # Generate strongly connected component of second-order networks
+    start = tm.clock()
+    g2 = t.igraphSecondOrder().components(mode="STRONG").giant()
+    
+    if mode == 'FIRSTORDER':
+        g2n = t.igraphFirstOrder().components(mode="STRONG").giant()
+    else:
+        g2n = t.igraphSecondOrderNull().components(mode="STRONG").giant()
+    
+    # Calculate transition matrices
+    T2 = Processes.RWTransitionMatrix(g2, sparseLA=True, transposed=True)
+    T2n = Processes.RWTransitionMatrix(g2n, sparseLA=True, transposed=True)
 
-        # Compute entropy growth rates of transition matrices        
-        H2 = np.absolute(EntropyGrowthRate(T2))
-        H2n = np.absolute(EntropyGrowthRate(T2n))
-        
-        end = tm.clock()
-        print('Time for EntropyGrowthRateRatio:', (end - start))
-        # Return ratio
-        return H2/H2n        
+    # Compute entropy growth rates of transition matrices        
+    H2 = np.absolute(EntropyGrowthRate(T2))
+    H2n = np.absolute(EntropyGrowthRate(T2n))
+    
+    end = tm.clock()
+    print('Time for EntropyGrowthRateRatio:', (end - start))
+    # Return ratio
+    return H2/H2n        
         
 
 def __Entropy(prob):
-        H = 0
-        for p in prob:
-            H = H+np.log2(p)*p
-        return -H
+    H = 0
+    for p in prob:
+        H = H+np.log2(p)*p
+    return -H
 
 def __BWPrefMatrix(t, v):
-    """Computes a betweenness preference matrix for a node v in a temporal network t"""
+    """Computes a betweenness preference matrix for a node v in a temporal network t
+    
+    @param t: The temporalnetwork instance to work on
+    @param v: Name of the node to compute its BetweennessPreference
+    """
     g = t.igraphFirstOrder()
     v_vertex = g.vs.find(name=v)
     indeg = v_vertex.degree(mode="IN")        
@@ -158,66 +192,72 @@ def __BWPrefMatrix(t, v):
     return B_v
 
 def BetweennessPreference(t, v, normalized = False):
-        """Computes the betweenness preference of a node v in a temporal network t"""
-        
-        g = t.igraphFirstOrder()
-        
-        # If the network is empty, just return zero
-        if len(g.vs) == 0:
-            return 0.0        
-        
-        v_vertex = g.vs.find(name=v)    
-        indeg = v_vertex.degree(mode="IN")        
-        outdeg = v_vertex.degree(mode="OUT")
+    """Computes the betweenness preference of a node v in a temporal network t
     
-        # First create the betweenness preference matrix (equation (2) of the paper)
-        B_v = __BWPrefMatrix(t, v)
-        
-        # Normalize matrix (equation (3) of the paper)
-        P_v = np.matrix(np.zeros(shape=(indeg, outdeg)))
-        S = 0.0
-        for s in range(indeg):
-            for d in range(outdeg):
-                S += B_v[s,d]
-        
-        if S>0:
-            for s in range(indeg):
-                for d in range(outdeg):
-                    P_v[s,d] = B_v[s,d]/S                    
-        
-        # Compute marginal probabilities
-        marginal_s = []
-        marginal_d = []
-        
-        # Marginal probabilities P^v_d = \sum_s'{P_{s'd}}
+    @param t: The temporalnetwork instance to work on
+    @param v: Name of the node to compute its BetweennessPreference
+    @param normalized: whether or not (default) to normalize
+    """
+    
+    g = t.igraphFirstOrder()
+    
+    # If the network is empty, just return zero
+    if len(g.vs) == 0:
+        return 0.0        
+    
+    # NOTE: this might raise a ValueError if vertex v is not found
+    v_vertex = g.vs.find(name=v)    
+    indeg = v_vertex.degree(mode="IN")        
+    outdeg = v_vertex.degree(mode="OUT")
+
+    # First create the betweenness preference matrix (equation (2) of the paper)
+    B_v = __BWPrefMatrix(t, v)
+    
+    # Normalize matrix (equation (3) of the paper)
+    P_v = np.matrix(np.zeros(shape=(indeg, outdeg)))
+    S = 0.0
+    for s in range(indeg):
         for d in range(outdeg):
-            P_d = 0.0
-            for s_prime in range(indeg):
-                P_d += P_v[s_prime, d]
-            marginal_d.append(P_d)
-        
-        # Marginal probabilities P^v_s = \sum_d'{P_{sd'}}
-        for s in range(indeg):
-            P_s = 0.0
-            for d_prime in range(outdeg):
-                P_s += P_v[s, d_prime]
-            marginal_s.append(P_s)
-        
-        H_s = __Entropy(marginal_s)
-        H_d = __Entropy(marginal_d)
-        
-        I = 0.0
-        # Here we just compute equation (4) of the paper ... 
+            S += B_v[s,d]
+    
+    if S>0:
         for s in range(indeg):
             for d in range(outdeg):
-                if B_v[s, d] != 0: # 0 * Log(0)  = 0
-                    # Compute Mutual information
-                    I += P_v[s, d] * np.log2(P_v[s, d] / (marginal_s[s] * marginal_d[d]))
-        
-        if normalized:
-            return I/np.min([H_s,H_d])
-        else:
-            return I
+                P_v[s,d] = B_v[s,d]/S                    
+    
+    # Compute marginal probabilities
+    marginal_s = []
+    marginal_d = []
+    
+    # Marginal probabilities P^v_d = \sum_s'{P_{s'd}}
+    for d in range(outdeg):
+        P_d = 0.0
+        for s_prime in range(indeg):
+            P_d += P_v[s_prime, d]
+        marginal_d.append(P_d)
+    
+    # Marginal probabilities P^v_s = \sum_d'{P_{sd'}}
+    for s in range(indeg):
+        P_s = 0.0
+        for d_prime in range(outdeg):
+            P_s += P_v[s, d_prime]
+        marginal_s.append(P_s)
+    
+    H_s = __Entropy(marginal_s)
+    H_d = __Entropy(marginal_d)
+    
+    I = 0.0
+    # Here we just compute equation (4) of the paper ... 
+    for s in range(indeg):
+        for d in range(outdeg):
+            if B_v[s, d] != 0: # 0 * Log(0)  = 0
+                # Compute Mutual information
+                I += P_v[s, d] * np.log2(P_v[s, d] / (marginal_s[s] * marginal_d[d]))
+    
+    if normalized:
+        return I/np.min([H_s,H_d])
+    else:
+        return I
 
 
 def SlowDownFactor(t):    
@@ -225,6 +265,8 @@ def SlowDownFactor(t):
     a diffusion process in the temporal network evolves on a second-order model 
     compared to a first-order model. This value captures the effect of order
     correlations on a diffusion process in the temporal network.
+    
+    @param t: The temporalnetwork instance to work on
     """
     g2 = t.igraphSecondOrder().components(mode="STRONG").giant()
     g2n = t.igraphSecondOrderNull().components(mode="STRONG").giant()
@@ -244,11 +286,16 @@ def SlowDownFactor(t):
 def EigenvectorCentrality(t, model='SECOND'):
     """Computes eigenvector centralities of nodes in the second-order networks, 
     and aggregates them to obtain the eigenvector centrality of nodes in the 
-    first-order network."""
+    first-order network.
+    
+    @param t: The temporalnetwork instance to work on
+    @param model: either C{"SECOND"} or C{"NULL"}, where C{"SECOND"} is the 
+      the default value."""
     
     start = tm.clock()
 
-    assert model == 'SECOND' or model == 'NULL'
+    if (model is "SECOND" or "NULL") == False:
+        raise ValueError("model must be one of \"SECOND\" or \"NULL\"")
 
     name_map = {}
 
@@ -286,9 +333,15 @@ def EigenvectorCentrality(t, model='SECOND'):
 def BetweennessCentrality(t, model='SECOND'):
     """Computes betweenness centralities of nodes in the second-order networks, 
     and aggregates them to obtain the betweenness centrality of nodes in the 
-    first-order network."""
+    first-order network.
+    
+    @param t: The temporalnetwork instance to work on
+    @param model: either C{"SECOND"} or C{"NULL"}, where C{"SECOND"} is the 
+      the default value.
+    """
 
-    assert model == 'SECOND' or model == 'NULL'
+    if (model is "SECOND" or "NULL") == False:
+        raise ValueError("model must be one of \"SECOND\" or \"NULL\"")
 
     name_map = {}
 
@@ -320,9 +373,15 @@ def BetweennessCentrality(t, model='SECOND'):
 def PageRank(t, model='SECOND'):
     """Computes PageRank of nodes in the second-order networks, 
     and aggregates them to obtain the PageRank of nodes in the 
-    first-order network."""
+    first-order network.
+    
+    @param t: The temporalnetwork instance to work on
+    @param model: either C{"SECOND"} or C{"NULL"}, where C{"SECOND"} is the 
+      the default value.
+    """
 
-    assert model == 'SECOND' or model == 'NULL'
+    if (model is "SECOND" or "NULL") == False:
+        raise ValueError("model must be one of \"SECOND\" or \"NULL\"")
 
     name_map = {}
 
